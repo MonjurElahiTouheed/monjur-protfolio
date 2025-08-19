99711b0de1efa0be6b4afd7244030fa6b2fe02e5
Monjur Elahi Touheed <monjurelahitouheed@gmail.com>
2025-08-19 20:20:40 +0600

    design navbar

---
+++ src/components/navbar/NavbarLogo.jsx
@@ -0,0 +1,14 @@
+const NavbarLogo = () => {
+  return (
+    <div>
+      <h1 className="text-white font-semibold text-2xl sm:hidden md:block">
+        Monjur <span className="text-orange">Elahi</span> Touheed
+      </h1>
+      <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
+        AES
+      </h1>
+    </div>
+  );
+};
+
+export default NavbarLogo;
