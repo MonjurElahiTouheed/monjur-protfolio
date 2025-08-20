65e71399f1d1e57dde7b096978421e7c924d4021
Monjur Elahi Touheed <monjurelahitouheed@gmail.com>
2025-08-20 12:14:36 +0600

    add primary hero pic

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
 import { PiHexagonThin } from "react-icons/pi";
-
+import myPic from '../../assets/edited photoshop .png'
 const HeroPic = () => {
   return (
     <motion.div
@@ -12,12 +12,12 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/amina.png"
+        src={myPic}
         alt="Amina Elahi Sarder"
         className="max-h-[400px] w-auto ml-2"
       />
 
-      <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
+      <div className="absolute -z-10 flex justify-center items-center animate-pulse">
         <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
       </div>
     </motion.div>
